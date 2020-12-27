caa83b742cbc4bf1c8390845521066b07a1f841b
HafiCode02 <hafizcode02@gmail.com>
2020-12-27 14:56:52 +0700

    adding UserGateway

--- app/Gateway/UserGateway.php
+++ app/Gateway/UserGateway.php
@@ -13,6 +13,28 @@ class UserGateway
 
     public function __construct()
     {
+        $this->db = app('db');
+    }
 
+    public function getUser(string $userId)
+    {
+        $user = $this->db->table('users')
+            ->where('user_id', $userId)
+            ->first();
+
+        if ($user) {
+            return (array) $user;
+        }
+
+        return null;
+    }
+
+    public function saveUser(string $userId, string $displayName)
+    {
+        $this->db->table('users')
+            ->insert([
+                'user_id' => $userId,
+                'display_name' => $displayName
+            ]);
     }
 }
